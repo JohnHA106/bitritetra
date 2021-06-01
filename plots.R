7882af2d91f86c3e83e2786ca5b9eaf11c72b38a
jobbie <jhunterallan@gmail.com>
2021-06-01 17:36:30 +0100

    some annotations added

--- plots.R
+++ plots.R
@@ -7,8 +7,15 @@ hexaose
 tetraose
 triose
 
-oligoplot <- ggplot(long, aes(x = time, y = nc, col = oligo)) + geom_line()
+oligoplot <- ggplot(long, aes(x = time, y = nc, col = oligo)) + geom_line(lwd = 1)
 oligoplot
 
 op2 <- oligoplot + xlim(c(7, 40)) + ylim(c(0, 750)) + theme_bw() + ylab("Charge difference, nC") + xlab("Time, mins")
 op2
+write.csv(long, file = "longtablerawdata.csv")
+##add in lines for standards
+op2 + geom_vline(xintercept = 15.3) + #biose
+  geom_vline(xintercept = 5.29) + #glucose
+  geom_vline(xintercept =  21.56) + #triose
+  geom_vline(xintercept = 28.86) + #hexaose
+  geom_vline(xintercept = 24.8) # tetraose
